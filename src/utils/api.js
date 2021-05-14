bb3618ef8a0625f942e48061d3d987e6a1b26163
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-14 15:10:54 -0600

    axios call in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,11 @@
 // getting api info from https://randomuser.me/documentation#howto using axios npm (make sure to install and import)
 
+import axios from "axios";
+
+//fetches users from API- only calling for 60 
+export default {
+    fetchUsers: function() {
+        return axios    
+            .get("https://randomuser.me/api/?results=60&nat=us")
+    }
+}
